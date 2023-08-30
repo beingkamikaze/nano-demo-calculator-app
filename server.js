ae31eba4cecf7d369bd270e91b845d53c8e6108b
Mayank Sandilya <78838306+beingkamikaze@users.noreply.github.com>
2023-08-31 00:18:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,23 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-
 const PORT = process.env.PORT || 8080;
-
-const baseUrl = "/calculator";
-
+const baseUrl = '/calculator'
 app.use(express.json());
-
 const baseRouter = express.Router();
 
-baseRouter.get("{baseUrl}/greeting", (req, res) => {
-  return res.status(200).send("Hello world!");
+baseRouter.get('/greeting', (req, res) => {
+    return res.send('Hello World!');
 });
 
-baseRouter.post("{baseUrl}/add", (req, res) => {
-  const { first, second } = req.body;
-  if (typeof first !== "number" || typeof second !== "number") {
-    res.status(400).send("Invalid Input");
-  } else {
-    const result = first + second;
-    res.status(200).json({ result: result });
-  }
+baseRouter.post('/add', (req, res) => {
+    const { first, second } = req.body;
+    res.json({ "result": first + second });
 });
 
-baseRouter.post("{baseUrl}/subtract", (req, res) => {
-  const { first, second } = req.body;
-  if (typeof first !== "number" || typeof second !== "number") {
-    res.status(400).send("Invalid Input");
-  } else {
-    const result = first - second;
-    res.status(200).json({ result: result });
-  }
+
+baseRouter.post('/subtract', (req, res) => {
+    const { first, second } = req.body;
+    res.json({ "result": first - second });
 });
 
 app.use(baseUrl, baseRouter);
-app.listen(PORT, () => {
-  console.log("Server running at PORT", PORT);
-});
